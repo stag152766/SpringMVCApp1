3d6de23f0c880e155af7d0cb2ebc88de7ae5d08c
stag152766 <45874547+stag152766@users.noreply.github.com>
2021-01-03 15:08:08 +0700

    add lesson17

---
+++ src/main/java/ru/alishev/springcourse/config/controllers/FirstController.java
@@ -0,0 +1,21 @@
+package ru.alishev.springcourse.config.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/first")
+public class FirstController {
+
+    @GetMapping("/hello")
+    public String helloPage(){
+        return "first/hello";
+    }
+
+    @GetMapping("/goodbye")
+    public String goobbuyPage(){
+        return "first/goodbye";
+    }
+}
