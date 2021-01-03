f9972a893dcd254047db4c7168119a0fd0af709f
stag152766 <45874547+stag152766@users.noreply.github.com>
2021-01-03 23:57:55 +0700

    add hw lesson20

--- src/main/java/ru/alishev/springcourse/config/controllers/FirstController.java
+++ src/main/java/ru/alishev/springcourse/config/controllers/FirstController.java
@@ -2,6 +2,7 @@ package ru.alishev.springcourse.config.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,8 +15,10 @@ public class FirstController {
 
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name",required = false) String name,
-                            @RequestParam(value = "surname",required = false) String surname){
-        System.out.println("Hello, " + name + " " + surname);
+                            @RequestParam(value = "surname",required = false) String surname,
+                            Model model){
+
+        model.addAttribute("message", "Hello, " + name + " " + surname);
         return "first/hello";
     }
 
@@ -23,4 +26,29 @@ public class FirstController {
     public String goobbuyPage(){
         return "first/goodbye";
     }
+
+
+    @GetMapping("/calculator")
+    public String calculate(@RequestParam("a") int a,
+                            @RequestParam("b") int b,
+                            @RequestParam("action") String action,
+                            Model model){
+
+        double result = 0;
+        if (action.equals("multiplication")){
+            result = a * b;
+        }
+        else if (action.equals("addition")){
+            result = a + b;
+        }
+        else if (action.equals("substraction")){
+            result = a - b;
+        }
+        else if (action.equals("division") && b != 0){
+            result = (double) a / b;
+        }
+        model.addAttribute("result", result);
+        return "first/calculator" ;
+    }
+
 }
